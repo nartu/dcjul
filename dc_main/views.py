86c3e9696b314e2de892026a929b1433432bd58a
Nartu <nowhereness.almeo@gmail.com>
2020-03-07 01:58:03 +0300

    view tag_list

--- dc_main/views.py
+++ dc_main/views.py
@@ -12,9 +12,8 @@ def tag_list(request):
     content = dict()
     print(tags)
     for tag in tags:
-        content[tag.pk] = []
-        media_ids = TagMediaBond.objects.filter(tag_id=tag.id)
-        # content[tag.pk] = media_ids
+        content[tag.pk] = {'tag': tag, 'media': []}
+        media_ids = TagMediaBond.objects.filter(tag_id=tag.pk)
         for media in media_ids:
-            content[tag.pk] += Media.objects.filter(pk=media.pk)
+            content[tag.pk]['media'] += Media.objects.filter(pk=media.pk)
     return render(request,'tag_list.html',{'tags': tags, 'content': content})
