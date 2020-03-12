0cbc6380b1f02928cf690da42646fe3230ecd5d4
Nartu <nowhereness.almeo@gmail.com>
2020-03-13 02:21:15 +0300

    View: media_detail + fancybox

--- dc_main/urls.py
+++ dc_main/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('tags', views.tags, name='tags'),
     path('tag/list', views.tag_list, name='tag_list'),
+    path('tag/<int:pk>', views.tag_detail, name='tag_detail'),
+    path('media/<int:pk>', views.media_detail, name='media_detail'),
 ]
--- dc_main/views.py
+++ dc_main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tag, Media, TagMediaBond
+from django.core.paginator import Paginator
+# from django.shortcuts import redirect
 
 def index(request):
     return render(request,'index.html',{})
@@ -9,11 +11,21 @@ def tags(request):
 
 def tag_list(request):
     tags = Tag.objects.order_by('pk')
+    tags_paginator = Paginator(tags,1)
+    page = request.GET.get('page')
+    tags_page = tags_paginator.get_page(page)
     content = dict()
-    print(tags)
-    for tag in tags:
+    for tag in tags_page:
         content[tag.pk] = {'tag': tag, 'media': []}
         media_ids = TagMediaBond.objects.filter(tag_id=tag.pk)
         for media in media_ids:
             content[tag.pk]['media'] += Media.objects.filter(pk=media.pk)
     return render(request,'tag_list.html',{'tags': tags, 'content': content})
+
+def tag_detail(request):
+    pass
+
+def media_detail(request,pk):
+    # media = Media.objects.filter(pk=pk)
+    media = get_object_or_404(Media,pk=pk)
+    return render(request,'media_detail.html',{'media': media})
