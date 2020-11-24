31d51c988570b0a477d3a9b2ac2b1aa32348a554
Julia <webdoxy@gmail.com>
2020-11-24 23:06:51 +0300

    front index, about

--- dc_gallery/urls.py
+++ dc_gallery/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'dc_gallery'
 urlpatterns = [
     path('', views.gallery, name='gallery'),
-    path('tag/<int:tag_pk>', views.gallery_tag, name='gallery_tag')
+    path('tag/<int:tag_pk>', views.gallery_tag, name='gallery_tag'),
+    # change path later
+    path('index', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
--- dc_gallery/views.py
+++ dc_gallery/views.py
@@ -2,12 +2,29 @@ from django.shortcuts import render, get_object_or_404, redirect
 from dc_main.models import Tag, Media, TagMediaBond
 from django.core.paginator import Paginator
 
+# Static pages
+
+def index(request):
+    return render(request,'dc_design/index.html',{
+        })
+
+def about(request):
+    return render(request,'dc_design/about.html',{
+        })
+
+def contact(request):
+    return render(request,'dc_design/contact.html',{
+        })
+
+
+# Gallery (media agregator)
 
 def gallery(request):
-    return render(request,'gallery.html',{
-        # 'form': form,
-        # 'debug': debug
-        })
+    return redirect('dc_gallery:gallery_tag', tag_pk=1)
+    # return render(request,'dc_design/gallery.html',{
+    #     # 'form': form,
+    #     # 'debug': debug
+    #     })
 
 def gallery_tag(request,tag_pk):
     tags = Tag.objects.order_by('pk')
@@ -16,8 +33,12 @@ def gallery_tag(request,tag_pk):
     media_ids = TagMediaBond.objects.filter(tag_id=tag_target.pk)
     for media in media_ids:
         media_list += Media.objects.filter(pk=media.pk)
-    return render(request,'gallery.html',{
+    # pages, objects per page
+    media_paginator = Paginator(media_list,4)
+    page = request.GET.get('page')
+    media_list_page = media_paginator.get_page(page)
+    return render(request,'dc_design/gallery.html',{
         'tags': tags,
         'tag_target': tag_target,
-        'media_list': media_list
+        'media_list': media_list_page
     })
