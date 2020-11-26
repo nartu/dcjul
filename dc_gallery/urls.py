8457cbb3f18841f4efd132a35cccb975a38c31e4
Julia <webdoxy@gmail.com>
2020-11-26 23:00:32 +0300

    front: media-details, gallery-zoom

--- dc_gallery/urls.py
+++ dc_gallery/urls.py
@@ -6,6 +6,7 @@ app_name = 'dc_gallery'
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('tag/<int:tag_pk>', views.gallery_tag, name='gallery_tag'),
+    path('media/<int:media_pk>', views.media_detail, name='media_detail'),
     # change path later
     path('index', views.index, name='index'),
     path('about', views.about, name='about'),
--- dc_gallery/views.py
+++ dc_gallery/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from dc_main.models import Tag, Media, TagMediaBond
+from dc_parse.models import MediaVkPhotoThumbnail
 from django.core.paginator import Paginator
 
 # Static pages
@@ -42,3 +43,15 @@ def gallery_tag(request,tag_pk):
         'tag_target': tag_target,
         'media_list': media_list_page
     })
+
+def media_detail(request,media_pk):
+    """ Media detail by ID or message 'not exits' """
+    # media = Media.objects.filter(pk=pk)
+    media = get_object_or_404(Media,pk=media_pk)
+    if MediaVkPhotoThumbnail.objects.filter(media_id=media_pk):
+        media_thumbnail =  MediaVkPhotoThumbnail.objects.get(media_id=media_pk).x
+    else:
+        media_thumbnail =  None
+    return render(request,'dc_design/media_detail.html',{
+    'media_thumbnail': media_thumbnail,
+    'media': media})
